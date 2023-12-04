d758d15b6c87382a76701117e7c0a728f24f02b4
Agnes McFarlin <115196648+oohtmeel1@users.noreply.github.com>
2023-12-04 11:38:17 -0500

    Add files via upload

--- resume_main_d.py
+++ resume_main_d.py
@@ -55,7 +55,6 @@ with placeholder.container():
 	with col2:
 		st.markdown('''<p class="little-big">While my career started out with me working in wet labs as a Chemist, performing various chemical
 		</p>''',unsafe_allow_html=True)
-with placeholder.container():
 	st.markdown(''' <p class="little-big"> and biochemical tests, and working with a 
 			variety of instruments.I always enjoyed poring over the massive amounts of data that were generated during the course of some interval by work processes. So, in pursuit of furthering my career 
 			interest with such activities, I have completed a Master’s program in Data Science at the 
