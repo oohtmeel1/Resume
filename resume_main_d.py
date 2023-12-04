b78e63d42ff2ac07a320a09f47d1f41176b309d8
Agnes McFarlin <115196648+oohtmeel1@users.noreply.github.com>
2023-12-04 09:17:17 -0500

    Add files via upload

--- resume_main_d.py
+++ resume_main_d.py
@@ -99,11 +99,10 @@ if check1:
 	with st.container():
 		st.write("This is inside the container")
 
-   # You can call any Streamlit command, including custom components:
 		st.bar_chart(np.random.randn(50, 3))
 
 		st.write("This is outside the container")	
-placeholder.empty()
+	placeholder.empty()
   
   
 if check2:
