6fb85a729b9f2fb07f2f3f4b8c9ddc4ce1413661
Agnes McFarlin <115196648+oohtmeel1@users.noreply.github.com>
2023-12-14 11:13:43 -0500

    Add files via upload

--- resume_main_d.py
+++ resume_main_d.py
@@ -59,8 +59,6 @@ with placeholder.container():
 
 
 sideb = st.sidebar
-st.sidebar.image("https://drive.google.com/uc?export=view&id=1udI3U3LLKbZBKp4LLh-dRteKGP6C-lpp",width=50)
-
 st.sidebar.markdown('<p class="big-font">Agnes McFarlin</p>',unsafe_allow_html=True)
 st.sidebar.write("Enjoyer of science in all forms.")
 st.sidebar.link_button("Github", "https://github.com/oohtmeel1")
